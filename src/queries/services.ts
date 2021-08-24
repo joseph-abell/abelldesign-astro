7a51c667e18119a5edb2436339865f332a430c08
Joe Abell <jsphabll@gmail.com>
2021-08-24 21:28:41 +0100

    fix whitespace

--- src/queries/services.ts
+++ src/queries/services.ts
@@ -1,23 +1,23 @@
 const services = `
   query {
     services {
-        updatedAt
-        id
-        title
-        stage
-        publishedAt
-        featuredLinkText
-        featuredDeck
-        featuredImage {
-            url
-            height
-        }
-        deck
-        createdAt
-        linkText
-        work {
-            slug
-        }
+      updatedAt
+      id
+      title
+      stage
+      publishedAt
+      featuredLinkText
+      featuredDeck
+      featuredImage {
+        url
+        height
+      }
+      deck
+      createdAt
+      linkText
+      work {
+        slug
+      }
     }
     servicesPages(first:1) {
       quote
