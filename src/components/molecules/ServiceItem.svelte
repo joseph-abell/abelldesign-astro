ab3c2e564dca4111e2bdf65eeab34db9cf642cd1
Joe Abell <jsphabll@gmail.com>
2021-08-29 11:49:24 +0100

    add check if work exists

--- src/components/molecules/ServiceItem.svelte
+++ src/components/molecules/ServiceItem.svelte
@@ -4,27 +4,29 @@
 	export let slugPrefix;
 </script>
 
-<li itemscope itemtype="https://schema.org/Service">
-	<a
-		class={item.oddEven === 0 ? 'data-item-link row' : 'data-item-link row-reverse'}
-		href={`${slugPrefix}/${item.work.slug}`}
-		style="--flexDirection: {item.oddEven === 0 ? 'row' : 'row-reverse'}"
-        itemprop="url"
-	>
-		<div class="data-item-text">
-			<h3 itemprop="name">{item.title}</h3>
-			<p itemprop="description">{item.featuredDeck} <SpanAsLink>{item.featuredLinkText}</SpanAsLink></p>
-		</div>
-
-		{#if item.featuredImage.url}
-			<div>
-				<div style="display: flex; align-items: center; justify-content: center;">
-					<img alt="" src={item.featuredImage.url} itemtype="image" />
-				</div>
+{#if item.work}
+	<li itemscope itemtype="https://schema.org/Service">
+		<a
+			class={item.oddEven === 0 ? 'data-item-link row' : 'data-item-link row-reverse'}
+			href={`${slugPrefix}/${item.work.slug}`}
+			style="--flexDirection: {item.oddEven === 0 ? 'row' : 'row-reverse'}"
+			itemprop="url"
+		>
+			<div class="data-item-text">
+				<h3 itemprop="name">{item.title}</h3>
+				<p itemprop="description">{item.featuredDeck} <SpanAsLink>{item.featuredLinkText}</SpanAsLink></p>
 			</div>
-		{/if}
-	</a>
-</li>
+
+			{#if item.featuredImage.url}
+				<div>
+					<div style="display: flex; align-items: center; justify-content: center;">
+						<img alt="" src={item.featuredImage.url} itemtype="image" />
+					</div>
+				</div>
+			{/if}
+		</a>
+	</li>
+{/if}
 
 <style>
 	.data-item-link {
